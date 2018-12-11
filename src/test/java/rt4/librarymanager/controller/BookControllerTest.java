2a14944a885da18d3b32ceb20eb411722ab63888
diep <xuandiep2389@gmail.com>
2018-12-11 16:25:14 +0700

    test mockMVC notnull

--- src/test/java/rt4/librarymanager/controller/BookControllerTest.java
+++ src/test/java/rt4/librarymanager/controller/BookControllerTest.java
@@ -2,17 +2,44 @@ package rt4.librarymanager.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import rt4.librarymanager.model.Book;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BookControllerTest {
+
+    @InjectMocks
+    private BookController bookController;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableHandlerMethodArgumentResolver;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
+
     }
 
     @Test
     public void testShowCreateBookPage(){
-        assertEquals(1, 2-1);
+        assertNotNull(mockMvc);
+        assertNotNull(bookController);
     }
 }
