6158bd02fec1b5c3ac9ddc6d9a46cf4e17e2d893
diep <xuandiep2389@gmail.com>
2018-12-11 15:01:49 +0700

    initial commit

---
+++ src/test/java/rt4/librarymanager/LibraryManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package rt4.librarymanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
