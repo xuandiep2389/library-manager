c16c239a5bb5551bd2ab6c459461e044ddb9011d
diep <xuandiep2389@gmail.com>
2018-12-11 15:42:34 +0700

    tao bookControllerTest

---
+++ src/test/java/rt4/librarymanager/controller/BookControllerTest.java
@@ -0,0 +1,18 @@
+package rt4.librarymanager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testShowCreateBookPage(){
+        assertEquals(1, 2-1);
+    }
+}
