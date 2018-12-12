c5489ee58bbba35e510514eeeb9017b03282a89b
diep <xuandiep2389@gmail.com>
2018-12-12 11:46:22 +0700

    rewrite test

--- src/test/java/rt4/librarymanager/controller/BookControllerTest.java
+++ src/test/java/rt4/librarymanager/controller/BookControllerTest.java
@@ -38,55 +38,90 @@ public class BookControllerTest {
 
     }
 
+//    @Test
+//    public void testShowBookList() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        mockMvc.perform(get("/book/list"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+////        .andExpect(model().attribute("artifact", book));
+//    }
+//    @Test
+//    public void testShowCreateBookPage() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        Book book = Book.builder().build();
+//        mockMvc.perform(get("/book/create"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    public void testCreateBook() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        Book book = Book.builder().build();
+//        mockMvc.perform(post("/book/create"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+//    }
+//
+//
+//    @Test
+//    public void testShowEditBookPage() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        mockMvc.perform(get("/book/edit/{id}"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    public void testEditBook() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        mockMvc.perform(post("/book/edit"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    public void testShowDeleteBookPage() throws Exception{
+//        assertNotNull(mockMvc);
+//        assertNotNull(bookController);
+//        mockMvc.perform(post("/book/delete"))
+//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+//    }
+
     @Test
-    public void testShowBookList() throws Exception{
+    public void showCreateBookPage() throws Exception{
         assertNotNull(mockMvc);
         assertNotNull(bookController);
-        mockMvc.perform(get("/book/list"))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-//        .andExpect(model().attribute("artifact", book));
-    }
-    @Test
-    public void testShowCreateBookPage() throws Exception{
-        assertNotNull(mockMvc);
-        assertNotNull(bookController);
-        Book book = Book.builder().build();
         mockMvc.perform(get("/book/create"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
 
     @Test
-    public void testCreateBook() throws Exception{
+    public void createBook() throws Exception {
         assertNotNull(mockMvc);
         assertNotNull(bookController);
-        Book book = Book.builder().build();
         mockMvc.perform(post("/book/create"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
 
-
-    @Test
-    public void testShowEditBookPage() throws Exception{
-        assertNotNull(mockMvc);
-        assertNotNull(bookController);
-        mockMvc.perform(get("/book/edit/{id}"))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
-
-    @Test
-    public void testEditBook() throws Exception{
-        assertNotNull(mockMvc);
-        assertNotNull(bookController);
-        mockMvc.perform(post("/book/edit"))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
-
-    @Test
-    public void testShowDeleteBookPage() throws Exception{
-        assertNotNull(mockMvc);
-        assertNotNull(bookController);
-        mockMvc.perform(post("/book/delete"))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
-
+//    @Test
+//    public void listBook() {
+//    }
+//
+//    @Test
+//    public void showEditForm() {
+//    }
+//
+//    @Test
+//    public void editBook() {
+//    }
+//
+//    @Test
+//    public void viewDeleteForm() {
+//    }
+//
+//    @Test
+//    public void deleteBook() {
+//    }
 }
