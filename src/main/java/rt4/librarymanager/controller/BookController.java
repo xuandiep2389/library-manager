597c3d6a7ea517fe7d7421a39cd52b580de2f718
diep <xuandiep2389@gmail.com>
2018-12-12 10:10:26 +0700

    hien thi duoc list book

--- src/main/java/rt4/librarymanager/controller/BookController.java
+++ src/main/java/rt4/librarymanager/controller/BookController.java
@@ -1,16 +1,18 @@
 package rt4.librarymanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import rt4.librarymanager.model.Book;
 import rt4.librarymanager.service.BookService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/book")
@@ -38,12 +40,18 @@ public class BookController {
             modelAndView.addObject("book", new Book());
             return modelAndView;
         }
-
     }
 
     @GetMapping("/list")
-    public ModelAndView showListBook(){
+    public ModelAndView listBook(@RequestParam("search") Optional<String> search, @PageableDefault(size = 3) Pageable pageable) {
+        Page<Book> books;
+        if (search.isPresent()) {
+            books = bookService.findAllByName(search.get(), pageable);
+        } else {
+            books = bookService.findAll(pageable);
+        }
         ModelAndView modelAndView = new ModelAndView("/book/list");
+        modelAndView.addObject("books", books);
         return modelAndView;
     }
 
